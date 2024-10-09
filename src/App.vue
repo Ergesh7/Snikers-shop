36b82b3292e6a20939b02d99cac287bad8de78ae
ergesh7 <aidaraliev.k@gmail.com>
2024-10-09 20:52:50 +0600

    cart

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,75 @@
+<!-- <script setup></script>
+
+
+
+<template>
+  <div class="bg-white rounded-[20px]">
+    <header
+      class="flex justify-between items-center py-[43px] border-b-gray-200 border-b px-[45px]"
+    >
+      <img src="./assets/1st-logo.png" alt="" />
+      <nav class="flex gap-[32px] item-center">
+        <div @click="" class="flex gap-[10px]">
+          <img src="./assets/cart-logo.svg" alt="" />
+          <span>1205 руб.</span>
+        </div>
+        <RouterLink class="flex gap-[10px]" to="/favorite">
+          <img src="./assets/heart-logo.svg" alt="" />
+          <span>Закладки</span>
+        </RouterLink>
+      </nav>
+    </header>
+
+    <div class="p-[60px]"><RouterView /></div>
+    
+  </div>
+</template>
+
+<style>
+body {
+  background: #e7f6ff;
+  padding: 85px;
+}
+</style> -->
+
+
+
+
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { ref } from "vue";
+
+const isOpen = ref(false);
+
+const changeIsOpen = () => {
+  isOpen.value = false;
+};
 </script>
 
 <template>
-<header class="flex items-center justify-between px-[45px] border-b-[2px] pb-[44px] pt-[45px]" >
-  <div class="flex gap-[16px]">
-    <img src="./assets/logo.svg" alt="">
-    <div >
-      <h2 class="font-bold text-[20px]">REACT SNEAKERS</h2>
-      <p class="font-normal	text-[14px] text-[#9D9D9D]">Магазин лучших кроссовок</p>
-    </div>
-  </div>
+  <div class="bg-white rounded-[20px]">
+    <header
+      class="flex justify-between items-center py-[43px] border-b-gray-200 border-b px-[45px]"
+    >
+      <img src="./assets/sneakers.svg" alt="" />
+      <nav class="flex gap-[32px] item-center">
+        <div class="flex gap-[10px]" @click="isOpen = true">
+          <img class="cursor-pointer" src="./assets/basket.svg" alt="" />
+          <span class="cursor-pointer">1205 руб.</span>
+        </div>
+        <RouterLink class="flex gap-[10px]" to="/favorite">
+          <img src="./assets/heart.svg" alt="" />
+          <span>Закладки</span>
+        </RouterLink>
+      </nav>
+    </header>
 
-  <div class="nav flex gap-[32px]">
-     <div class="cart flex gap-[10px]">
-      <img src="./assets/cart-logo.svg" alt="">
-      <p>rub</p>
-     </div>
-     <div class="fav flex gap-[10px]">
-      <img src="./assets/heart-logo.svg" alt="">
-      <p>закладки</p>
-     </div>
+    <div class="p-[60px]"><RouterView :isOpen="isOpen" @bob="changeIsOpen" /></div>
   </div>
-
-  
-</header>
-  <!-- <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-  </nav> -->
-  <RouterView />
 </template>
 
-<style scoped>
+<style>
+body {
+  background: #e7f6ff;
+  padding: 85px;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,53 +1,225 @@
 <script setup>
+import img1 from "../assets/nike-1.png";
+import img2 from "../assets/nike-2.png";
+
+import img3 from "../assets/nike-3.png";
+
+import img4 from "../assets/nike-4.png";
+import img5 from "../assets/nike-5.png";
+import img6 from "../assets/nike-6.png";
+import img7 from "../assets/nike-7.png";
+import img8 from "../assets/nike-8.png";
+import img9 from "../assets/nike-9.png";
+import img10 from "../assets/nike-10.png";
+import img11 from "../assets/nike-11.png";
+import img12 from "../assets/nike-12.jpg";
+
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Navigation, Pagination, A11y } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
+import Sneaker from '../components/Sneaker.vue';
+import { ref } from "vue";
+
+defineProps(['isOpen']);
+const emit = defineEmits();
+
+const sneakers = [
+  {
+    id: 0,
+    sneakersUrl: img1,
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 12999,
+  },
+  {
+    id: 1,
+    sneakersUrl: img2,
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 12999,
+  },
+  {
+    id: 2,
+    sneakersUrl: img3,
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8499,
+  },
+  {
+    id: 3,
+    sneakersUrl: img4,
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 8999,
+  },
+  {
+    id: 4,
+    sneakersUrl: img5,
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 8999,
+  },
+  {
+    id: 5,
+    sneakersUrl: img6,
+    title: "Мужские Кроссовки Under Armour Curry 8",
+    price: 15199,
+  },
+  {
+    id: 6,
+    sneakersUrl: img7,
+    title: "Мужские Кроссовки Nike Kyrie 7",
+    price: 11299,
+  },
+  {
+    id: 7,
+    sneakersUrl: img8,
+    title: "Мужские Кроссовки Jordan Air Jordan 11",
+    price: 10799,
+  },
+  {
+    id: 8,
+    sneakersUrl: img9,
+    title: "Мужские Кроссовки Nike LeBron XVIII",
+    price: 16499,
+  },
+  {
+    id: 9,
+    sneakersUrl: img10,
+    title: "Мужские Кроссовки Nike Lebron XVIII Low",
+    price: 13999,
+  },
+  {
+    id: 10,
+    sneakersUrl: img11,
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8499,
+  },
+  {
+    id: 11,
+    sneakersUrl: img12,
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 11299,
+  },
+];
+
+const liked = ref([]);
+const cart = ref([]);
+
+const addToLiked = (e) => {
+  liked.value.push(e);
+};
+
+const removeFromLiked = (e) => {
+  liked.value = liked.value.filter((item) => item.id !== e.id);
+};
+
+const addToCart = (e) => {
+  cart.value.push(e);
+}
+
+const removeFromCart = (e) => {
+  cart.value = cart.value.filter(item => item.id !==e.id);
+}
+
+const changeIsOpen =()=>{
+    emit('bob', false)
+}
 </script>
 
-<template>
-  <div>
-    <p></p>
-  </div>
 
-  <Swiper
-    :modules="[Navigation, Pagination]"
+
+
+
+<template>
+    <div
+    class="popup bg-black/[0.5] h-[100vh] fixed z-20 top-0 left-0 w-full flex justify-end"
+    v-if="isOpen"
+  >
+    <div class="h-[100%] w-[385px] bg-white p-[32px] flex flex-col">
+      <p class="font-bold text-[24px] mb-[30px] flex justify-between">
+        Корзина 
+        <img class="cursor-pointer"
+        @click="changeIsOpen"
+        src="../assets/close.svg" alt="" />
+      </p>
+      <div class="flex flex-col gap-[20px] flex-1 overflow-y-auto">
+        <div
+          class="border-[1px] border-[#F3F3F3] rounded-[20px] flex p-[20px] gap-[20px]"
+          v-for="(sneaker, i) in sneakers.slice(0, 5)"
+          :key="i"
+        >
+          <img class="h-[70px] w-[70px]" :src="sneaker.sneakersUrl" alt="" />
+          <div>
+            <h2 class="font-normal text-[14px]">
+              {{ sneaker.title }}
+            </h2>
+            <p class="font-bold text-[14px]">{{ sneaker.price }} с.</p>
+          </div>
+          <img src="../assets/close.svg" alt="" />
+        </div>
+      </div>
+      <div>
+        <div class="flex">
+          <h3>Итого:</h3>
+          <div class="flex-1 border-dashed border-b-[1px]"></div>
+          <p class="font-semibold text-[16px]">21 498 c.</p>
+        </div>
+        <div class="flex mt-[15px]">
+          <h3>Налог 5%:</h3>
+          <div class="flex-1 border-dashed border-b-[1px]"></div>
+          <p class="font-semibold">1074 c.</p>
+        </div>
+        <button
+          class="bg-[#9DD458] flex text-white w-full py-[18px] justify-center rounded-[18px] font-semibold text-[16px] items-center relative mt-[24px]"
+        >
+          <span class="">Оформить заказ</span>
+          <img class="absolute right-[29px]" src="../assets/arrow.svg" alt="" />
+        </button>
+      </div>
+    </div>
+  </div>
+ <Swiper
     :slides-per-view="1"
     :space-between="50"
     :pagination="{ clickable: true }"
   >
     <SwiperSlide>
-      <img src="../assets/frog.png" alt="" />
+      <div class="flex justify-center my-[45px]">
+        <img src="../assets/frog.png" alt="" />
+      </div>
     </SwiperSlide>
     <SwiperSlide>
-      <img src="../assets/frog.png" alt="" />
+      <div class="flex justify-center my-[45px]">
+        <img src="../assets/frog.png" alt="" />
+      </div>
     </SwiperSlide>
     <SwiperSlide>
-      <img src="../assets/frog.png" alt="" />
+      <div class="flex justify-center my-[45px]">
+        <img src="../assets/frog.png" alt="" />
+      </div>
     </SwiperSlide>
   </Swiper>
 
-  <div class="flex justify-between mx-[60px] items-center mt-[40px]">
-    <p>Все кроссовки</p>
-    <div class="flex">
-      <img src="../assets/search.svg" alt="" />
-      <input type="text" class="border-[#F3F3F3] border-t-[45px] placeholder" />
+  <div class="">
+    <div class="flex justify-between">
+      <h2 class="font-bold text-[32px]">Все кроссовки</h2>
+      <label
+        for="search"
+        class="flex gap-[13.75px] w-[250px] py-[13px] pl-[18px] rounded-[10px] border-gray-200 border-[1px]"
+      >
+        <input type="text" placeholder="Поиск..." id="search" />
+      </label>
     </div>
-  </div>
 
-  <div class="mt-[30px] flex flex-wrap gap-[25px]">
-    <div class="">
-      <img src="../assets/nike-1.png" alt="" />
-      <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
-      <div class="flex items-center">
-        <div>
-          <p>Цена:</p>
-          <p>12 999</p>
-        </div>
-        <img src="../assets/Vector.svg" alt="" />
-      </div>
+    <div class="content grid grid-cols-4 gap-y-[40px] mt-[40px]">
+      <Sneaker
+        v-for="(sneaker, i) in sneakers"
+        :key="i"
+        :sneaker="sneaker"
+        @jazgul="addToLiked"
+        @anjelika="removeFromLiked"
+        @addToCart="addToCart"
+        @removeFromCart="removeFromCart"
+      />
     </div>
   </div>
 </template>
